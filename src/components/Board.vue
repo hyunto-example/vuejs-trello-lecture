7e7a8a14a659882355e7cb4e4b57948a74e6d2db
hyunto <jhyunto@gmail.com>
2019-04-29 23:29:17 +0900

    2. 라우팅 - 동적 라우트 매칭
    - this.$route 사용법 확인
    - <router-link to="">를 통한 동적 라우트 매칭 실습

---
+++ src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Board
+        <div>bid: {{bid}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.bid = this.$route.params.bid
+    },
+    data() {
+        return {
+            bid: 0
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
