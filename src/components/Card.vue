06247be102ac666492f70c624866c37abf45a7fd
hyunto <jhyunto@gmail.com>
2019-04-30 00:02:53 +0900

    2. 라우팅 - 중첩 라우트
    - router-view를 사용한 중첩 라우트 실습

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        Card
+        <div>
+            cid: {{cid}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.cid = this.$route.params.cid
+    },
+    watch: {
+        '$route'() {
+            this.cid = this.$route.params.cid
+        }
+    },
+    data() {
+        return {
+            cid: 0
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
